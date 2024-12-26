f93b583b63f7486fd4ad1aa5edad0fe1f3f0dbe7
FLinLan <maxlan0987@gmail.com>
2024-12-26 23:02:39 +0800

    added chromedriver and web driver example code

--- scrape.py
+++ scrape.py
@@ -5,8 +5,8 @@ import time
 def scrape_website(website):
     print("Launching chrome browser...")
 
-    chrome_driver_path = "./ChromeDriver"
-    options = webdriver.ChromOptions()
+    chrome_driver_path = "./chromedriver"
+    options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
     try:
@@ -18,3 +18,23 @@ def scrape_website(website):
         return html
     finally:
         driver.quit()
+
+
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from selenium.webdriver.common.by import By
+AUTH = 'brd-customer-hl_080580da-zone-ai_scraper:0x11bo7upg7t'
+SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
+def main():
+    print('Connecting to Scraping Browser...')
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        print('Connected! Navigating...')
+        driver.get('https://example.com')
+        print('Taking page screenshot to file page.png')
+        driver.get_screenshot_as_file('./page.png')
+        print('Navigated! Scraping page content...')
+        html = driver.page_source
+        print(html)
+if __name__ == '__main__':
+  main()
